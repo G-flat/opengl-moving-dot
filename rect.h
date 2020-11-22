613099fb7487418d0ea4a7f3e05fe4a578117963
G-flat <63449095+G-flat@users.noreply.github.com>
2020-11-22 21:55:54 +0800

    Turn tabs into spaces

--- rect.h
+++ rect.h
@@ -7,70 +7,70 @@
 
 typedef struct GL_RECT
 {
-	LONG x_L;
-	LONG y_T;
-	LONG x_R;
-	LONG y_B;
+    LONG x_L;
+    LONG y_T;
+    LONG x_R;
+    LONG y_B;
 } GL_RECT;
 
 /* Converting client coordinates to world coordinates */
 double CLX_TO_WRX(LONG x, LONG screen_width)
 {
-	return (-1.0 + (2.0 * x) / screen_width);
+    return (-1.0 + (2.0 * x) / screen_width);
 }
 
 double CLY_TO_WRY(LONG y, LONG screen_height)
 {
-	return (-1.0 + (2.0 * y) / screen_height);
+    return (-1.0 + (2.0 * y) / screen_height);
 }
 
 // Set the coordinates of a rectangle
 void setRect(GL_RECT *rect, LONG x_L, LONG y_T, LONG x_R, LONG y_B)
 {
-	(*rect).x_L = x_L;
-	(*rect).y_T = y_T;
-	(*rect).x_R = x_R;
-	(*rect).y_B = y_B;
+    (*rect).x_L = x_L;
+    (*rect).y_T = y_T;
+    (*rect).x_R = x_R;
+    (*rect).y_B = y_B;
 }
 
 // Move a rectangle by a specific number of pixels within the bounds of a window
 void moveRect(GL_RECT *rect, LONG xOff, LONG yOff, LONG screen_width, LONG screen_height)
 {
-	LONG x_T = (*rect).x_L + xOff;
-	LONG x_R = (*rect).x_R + xOff;
-	LONG y_T = (*rect).y_T + yOff;
-	LONG y_B = (*rect).y_B + yOff;
+    LONG x_T = (*rect).x_L + xOff;
+    LONG x_R = (*rect).x_R + xOff;
+    LONG y_T = (*rect).y_T + yOff;
+    LONG y_B = (*rect).y_B + yOff;
 
-	if (BETWEEN(x_T, 0, screen_width) && BETWEEN(x_R, 0, screen_width) && BETWEEN(y_T, 0, screen_height) && BETWEEN(y_B, 0, screen_height))
-		setRect(rect, x_T, y_T, x_R, y_B);
+    if (BETWEEN(x_T, 0, screen_width) && BETWEEN(x_R, 0, screen_width) && BETWEEN(y_T, 0, screen_height) && BETWEEN(y_B, 0, screen_height))
+        setRect(rect, x_T, y_T, x_R, y_B);
 }
 
 // Draw a rectangle inside a window using 2 triangles
 void drawRect(LONG cx_L, LONG cy_T, LONG cx_R, LONG cy_B, LONG screen_width, LONG screen_height, LONG colour)
 {
-    glClear(GL_COLOR_BUFFER_BIT);	// Clear the window
-    glBegin(GL_TRIANGLES);			// Draw triangles
+    glClear(GL_COLOR_BUFFER_BIT);  // Clear the window
+    glBegin(GL_TRIANGLES);         // Draw triangles
 
-	// Set the colour
+    // Set the colour
     glColor3ub((colour & 0xFF0000) >> 16, (colour & 0x00FF00) >> 8, (colour & 0x0000FF));
 
-	// Translate client coordinates to world coordinates
-	double wx_L = CLX_TO_WRX(cx_L, screen_width);
-	double wx_R = CLX_TO_WRX(cx_R, screen_width);
-	double wy_T = CLY_TO_WRY(cy_T, screen_height);
-	double wy_B = CLY_TO_WRY(cy_B, screen_height);
+    // Translate client coordinates to world coordinates
+    double wx_L = CLX_TO_WRX(cx_L, screen_width);
+    double wx_R = CLX_TO_WRX(cx_R, screen_width);
+    double wy_T = CLY_TO_WRY(cy_T, screen_height);
+    double wy_B = CLY_TO_WRY(cy_B, screen_height);
 
-	// 1st triangle
-    glVertex2d(wx_L, wy_T); // Top left
-    glVertex2d(wx_R, wy_T); // Top right
-	glVertex2d(wx_L, wy_B); // Bottom left
+    // 1st triangle
+    glVertex2d(wx_L, wy_T);  // Top left
+    glVertex2d(wx_R, wy_T);  // Top right
+    glVertex2d(wx_L, wy_B);  // Bottom left
 
-	// 2nd triangle
-	glVertex2d(wx_L, wy_B); // Bottom left
-    glVertex2d(wx_R, wy_B); // Bottom right
-	glVertex2d(wx_R, wy_T); // Top right
+    // 2nd triangle
+    glVertex2d(wx_L, wy_B);  // Bottom left
+    glVertex2d(wx_R, wy_B);  // Bottom right
+    glVertex2d(wx_R, wy_T);  // Top right
 
-	// Show the results
+    // Show the results
     glEnd();
     glFlush();
 }
--- window.c
+++ window.c
@@ -15,141 +15,141 @@ GL_RECT dot;
 // Callback for arrow keys
 void onArrowKeyPress(WPARAM vkCode)
 {
-	switch (vkCode)
-	{
-		case VK_LEFT:	moveRect(&dot, -RECT_WIDTH / 2,                0, SCREEN_WIDTH, SCREEN_HEIGHT); break;
-		case VK_RIGHT:	moveRect(&dot,  RECT_WIDTH / 2,                0, SCREEN_WIDTH, SCREEN_HEIGHT); break;
-		case VK_DOWN:	moveRect(&dot,               0, -RECT_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT); break;
-		case VK_UP:		moveRect(&dot,               0,  RECT_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT); break;
-	}
+    switch (vkCode)
+    {
+        case VK_LEFT:  moveRect(&dot, -RECT_WIDTH / 2,                0, SCREEN_WIDTH, SCREEN_HEIGHT); break;
+        case VK_RIGHT: moveRect(&dot,  RECT_WIDTH / 2,                0, SCREEN_WIDTH, SCREEN_HEIGHT); break;
+        case VK_DOWN:  moveRect(&dot,               0, -RECT_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT); break;
+        case VK_UP:    moveRect(&dot,               0,  RECT_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT); break;
+    }
 }
 
 // Set up the pixel format for the window
 void setupPixelFormat(HDC hdc)
 {
-	PIXELFORMATDESCRIPTOR pfd = {sizeof(PIXELFORMATDESCRIPTOR), 1,
-										PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL,
-										PFD_TYPE_RGBA,								// RGBA or palettes?
-										32,											// Colour depth
-										0, 0, 0, 0, 0, 0, 0, 0,						// Bitplanes in RGBA colour buffer
-										0, 0, 0, 0, 0,								// Bitplanes in accumulation buffer
-										24,											// z-axis buffer depth
-										8,											// Stencil buffer depth
-										0, 0, 0, 0, 0, 0};
-	int pixel_format = ChoosePixelFormat(hdc, &pfd);
-	SetPixelFormat(hdc, pixel_format, &pfd);
-	DescribePixelFormat(hdc, pixel_format, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
+    PIXELFORMATDESCRIPTOR pfd = {sizeof(PIXELFORMATDESCRIPTOR), 1,
+                                        PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL,
+                                        PFD_TYPE_RGBA,                            // RGBA or palettes?
+                                        32,                                       // Colour depth
+                                        0, 0, 0, 0, 0, 0, 0, 0,                   // Bitplanes in RGBA colour buffer
+                                        0, 0, 0, 0, 0,                            // Bitplanes in accumulation buffer
+                                        24,                                       // z-axis buffer depth
+                                        8,                                        // Stencil buffer depth
+                                        0, 0, 0, 0, 0, 0};
+    int pixel_format = ChoosePixelFormat(hdc, &pfd);
+    SetPixelFormat(hdc, pixel_format, &pfd);
+    DescribePixelFormat(hdc, pixel_format, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
 }
 
 // Callback for the message loop
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	static HDC hdc;
-	static HGLRC hglrc;
-	static PAINTSTRUCT ps;
+    static HDC hdc;
+    static HGLRC hglrc;
+    static PAINTSTRUCT ps;
 
-	switch (msg)
-	{
-		case WM_PAINT:
-		{
-			drawRect(dot.x_L, dot.y_T, dot.x_R, dot.y_B, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+    switch (msg)
+    {
+        case WM_PAINT:
+        {
+            drawRect(dot.x_L, dot.y_T, dot.x_R, dot.y_B, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
 
-			// These 2 functions are necessary to clear WM_PAINT messages from the queue
-			BeginPaint(hwnd, &ps);
-			EndPaint(hwnd, &ps);
-			break;
-		}
-		case WM_KEYDOWN:
-		{
-			if (BETWEEN(wParam, VK_LEFT, VK_DOWN))	// If an arrow key's pressed
-			{
-				onArrowKeyPress(wParam);
-				PostMessage(hwnd, WM_PAINT, 0, 0);	// Re-paint the window
-			}
-			break;
-		}
-		case WM_SIZE:
-		{
-			glViewport(0, 0, LOWORD(lParam), HIWORD(lParam));
-			PostMessage(hwnd, WM_PAINT, 0, 0);	// Re-paint the window
-			break;
-		}
-		case WM_DESTROY:
-		{
-			wglMakeCurrent(hdc, NULL);		// De-select the OpenGL context
-			wglDeleteContext(hglrc);		// Delete the context
-			PostQuitMessage(0);
-			break;
-		}
-		case WM_CLOSE:
-		{
-			DestroyWindow(hwnd);
-			break;
-		}
-		case WM_CREATE:
-		{						 
-			hdc = GetDC(hwnd);				// Get the device context for the window
-			setupPixelFormat(hdc);			// Set up the pixel format for the device context
+            // These 2 functions are necessary to clear WM_PAINT messages from the queue
+            BeginPaint(hwnd, &ps);
+            EndPaint(hwnd, &ps);
+            break;
+        }
+        case WM_KEYDOWN:
+        {
+            if (BETWEEN(wParam, VK_LEFT, VK_DOWN))  // If an arrow key's pressed
+            {
+                onArrowKeyPress(wParam);
+                PostMessage(hwnd, WM_PAINT, 0, 0);  // Re-paint the window
+            }
+            break;
+        }
+        case WM_SIZE:
+        {
+            glViewport(0, 0, LOWORD(lParam), HIWORD(lParam));
+            PostMessage(hwnd, WM_PAINT, 0, 0);  // Re-paint the window
+            break;
+        }
+        case WM_DESTROY:
+        {
+            wglMakeCurrent(hdc, NULL);  // De-select the OpenGL context
+            wglDeleteContext(hglrc);    // Delete the context
+            PostQuitMessage(0);
+            break;
+        }
+        case WM_CLOSE:
+        {
+            DestroyWindow(hwnd);
+            break;
+        }
+        case WM_CREATE:
+        {                         
+            hdc = GetDC(hwnd);              // Get the device context for the window
+            setupPixelFormat(hdc);          // Set up the pixel format for the device context
 
-			hglrc = wglCreateContext(hdc);	// Create an OpenGL context
-			wglMakeCurrent(hdc, hglrc);		// Make the context current
+            hglrc = wglCreateContext(hdc);  // Create an OpenGL context
+            wglMakeCurrent(hdc, hglrc);     // Make the context current
 
-			setRect(&dot, (SCREEN_WIDTH - RECT_WIDTH) / 2, (SCREEN_HEIGHT - RECT_HEIGHT) / 2, (SCREEN_WIDTH + RECT_WIDTH) / 2, (SCREEN_HEIGHT + RECT_HEIGHT) / 2);
-			break;
-		}
-		default:
-			return DefWindowProc(hwnd, msg, wParam, lParam);
-	}
-	return 0;
+            setRect(&dot, (SCREEN_WIDTH - RECT_WIDTH) / 2, (SCREEN_HEIGHT - RECT_HEIGHT) / 2, (SCREEN_WIDTH + RECT_WIDTH) / 2, (SCREEN_HEIGHT + RECT_HEIGHT) / 2);
+            break;
+        }
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
 }
 
 // Entry point (analogous to 'int main()')
 int WINAPI WinMainCRTStartup()
 {
-	// Get the handle of the current process
-	HMODULE hInstance = GetModuleHandleA(NULL);
+    // Get the handle of the current process
+    HMODULE hInstance = GetModuleHandleA(NULL);
 
-	// Initialize the window class
-	WNDCLASSEXA windowClass = {sizeof(WNDCLASSEX),
-	                           CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
-	                           WindowProc,
-	                           0, 0,
-	                           hInstance,
-	                           NULL,								// Handle to class icon
-	                           LoadCursorA(NULL, IDC_ARROW),		// Handle to class cursor
-	                           NULL,								// Handle to background brush
-	                           NULL,
-	                           "a",
-	                           NULL};								// Handle to small icon
+    // Initialize the window class
+    WNDCLASSEXA windowClass = {sizeof(WNDCLASSEX),
+                               CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
+                               WindowProc,
+                               0, 0,
+                               hInstance,
+                               NULL,                                // Handle to class icon
+                               LoadCursorA(NULL, IDC_ARROW),        // Handle to class cursor
+                               NULL,                                // Handle to background brush
+                               NULL,
+                               "a",
+                               NULL};                               // Handle to small icon
 
-	// Register the window class
-	EXIT_IF(!RegisterClassExA(&windowClass));
+    // Register the window class
+    EXIT_IF(!RegisterClassExA(&windowClass));
 
-	// Create the window
-	HWND window = CreateWindowExA(0,								// Extended styles
-		                          "a",								// Window class name
-		                          "Moving dot",						// Window title
-		                          WS_OVERLAPPEDWINDOW,				// Window style
-		                          CW_USEDEFAULT, CW_USEDEFAULT,		// Position
-								  SCREEN_WIDTH, SCREEN_HEIGHT,		// Size
-		                          NULL, NULL, hInstance, NULL);
-	EXIT_IF(!window);
+    // Create the window
+    HWND window = CreateWindowExA(0,                             // Extended styles
+                                  "a",                           // Window class name
+                                  "Moving dot",                  // Window title
+                                  WS_OVERLAPPEDWINDOW,           // Window style
+                                  CW_USEDEFAULT, CW_USEDEFAULT,  // Position
+                                  SCREEN_WIDTH, SCREEN_HEIGHT,   // Size
+                                  NULL, NULL, hInstance, NULL);
+    EXIT_IF(!window);
 
-	// Show the window
-	ShowWindow(window, SW_SHOW);
-	UpdateWindow(window);
+    // Show the window
+    ShowWindow(window, SW_SHOW);
+    UpdateWindow(window);
 
-	// Run the message loop
-	MSG msg = {0};
-	BOOL ret;
-	while ((ret = GetMessage(&msg, NULL, 0, 0)) != 0)
-	{
-		if (ret == -1)	// Error!
-			break;
-		TranslateMessage(&msg);
-		DispatchMessage(&msg); 
-	}
+    // Run the message loop
+    MSG msg = {0};
+    BOOL ret;
+    while ((ret = GetMessage(&msg, NULL, 0, 0)) != 0)
+    {
+        if (ret == -1)  // Error!
+            break;
+        TranslateMessage(&msg);
+        DispatchMessage(&msg); 
+    }
 
-	// Stop the program (analogous to 'return 0')
-	ExitProcess(0);
+    // Stop the program (analogous to 'return 0')
+    ExitProcess(0);
 }
